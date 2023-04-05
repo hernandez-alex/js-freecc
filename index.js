9ae7fc032cad1f2ffb3de520c1fff40a9739031d
Alex Hernandez <62507090+hernandezaw@users.noreply.github.com>
2023-04-05 08:38:51 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,86 @@
+"use strict";
+// ****************************************************************************
+// Set Default Parameters for Your Functions
+// ****************************************************************************
+
+/*
+// ****************************************************************************
+// Set Default Parameters for Your Functions
+// ****************************************************************************
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+console.log(increment(9));
+
+// ****************************************************************************
+// Write Arrow Functions with Parameters
+// ****************************************************************************
+//var myConcat = function(arr1, arr2) {
+//  return arr1.concat(arr2);
+//};
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+// ****************************************************************************
+// Use Arrow Functions to Write Concise Anonymous Functions
+// ****************************************************************************
+//var magic = function() {
+//  return new Date();
+//};
+
+const magic = () => new Date();
+console.log(magic());
+
+// ****************************************************************************
+// Mutate an Array Declared with const
+// ****************************************************************************
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+  // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+  // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+console.log(PI);
+
+// ****************************************************************************
+// Mutate an Array Declared with const
+// ****************************************************************************
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Using s = [2, 5, 7] would be invalid
+  console.log(s);
+  // Only change code above this line
+}
+editInPlace();
+
+// ****************************************************************************
+// Compare Scopes of the var and let Keywords
+// ****************************************************************************
+function checkScope() {
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+checkScope();
+
 // ****************************************************************************
 // Use Recursion to Create a Range of Numbers
 // ****************************************************************************
@@ -10,7 +93,6 @@ function rangeOfNumbers(startNum, endNum) {
   //   return numbers;
   // }
   if (endNum < startNum) return [];
-
   const numbers = rangeOfNumbers(startNum, endNum - 1);
   numbers.push(endNum);
   return numbers;
@@ -18,7 +100,6 @@ function rangeOfNumbers(startNum, endNum) {
 console.log(rangeOfNumbers(4, 9));
 console.log(`Alex que mariquera, estas aprendiendo`);
 
-/*
 // ****************************************************************************
 // Utiliza recursión para crear una cuenta regresiva
 // ****************************************************************************
