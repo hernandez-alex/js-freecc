d0993329753c51d15f256ed63698c477bdba871c
Alex Hernandez (M) <128613758+hernandez-alex@users.noreply.github.com>
2023-04-14 13:22:45 -0400

    finish alg-scripting.js

--- alg-scripting.js
+++ alg-scripting.js
@@ -1,21 +1,137 @@
 "use strict";
+
+/*
 // ****************************************************************************
-//
+// Chunky Monkey
 // ****************************************************************************
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  let newFile = [];
+  for (let i = 0; i < arr.length; i++) {
+    newFile.push(arr[i]);
+    if (newFile.length % size === 0) {
+      newArr.push(newFile);
+      newFile = [];
+    }
+  }
+  if (newFile.length > 0) newArr.push(newFile);
+  return newArr;
+}
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+
+// ****************************************************************************
+// Mutations
+// ****************************************************************************
+function mutation(arr) {
+  const Base = arr[1].toLowerCase().split("");
+  const texto = arr[0].toLowerCase().split("");
+  let result = true;
+  for (let i = 0; i < Base.length; i++) {
+    if (!texto.includes(Base[i])) result = false;
+  }
+  return result;
+}
+console.log(mutation(["hello", "hey"]));
+
+// ****************************************************************************
+// Where do I Belong
+// ****************************************************************************
+function getIndexToIns(arr, num) {
+  const arrSort = arr.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(arrSort);
+  for (let i = arrSort.length - 1; i >= 0; i--) {
+    if (num > arrSort[i]) return i+1;
+  }
+  return 0;
+}
+getIndexToIns([40, 60], 50);
+// ****************************************************************************
+// Falsy Bouncer
+// ****************************************************************************
+function bouncer(arr) {
+  const arrNotFalsy = [...arr];
+  const falsyVal = [false, null, 0, "", undefined, NaN];
+  for (let i = arrNotFalsy.length - 1; i >= 0; i--) {
+    if (falsyVal.includes(arrNotFalsy[i])) {
+      arrNotFalsy.splice(i, 1);
+    }
+  }
+  return arrNotFalsy;
+}
+bouncer([7, "ate", "", false, 9]);
+
+// ****************************************************************************
+// Slice and Splice
+// ****************************************************************************
+function frankenSplice(arr1, arr2, n) {
+  const arr3 = [...arr2];
+  arr3.splice(n,0, ...arr1)
+  return arr3;
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+// ****************************************************************************
+// Title Case a Sentence
+// ****************************************************************************
+function titleCase(str) {
+  const arrStr = str.split(" ");
+  for (let i = 0; i < arrStr.length; i++) {
+    arrStr[i] =
+      arrStr[i].slice(0, 1).toUpperCase() + arrStr[i].toLowerCase().slice(1, arrStr[i].length);
+  }
+  return arrStr.join(" ");
+}
+titleCase("I'm a little tea pot");
+
+// ****************************************************************************
+// Boo who
+// ****************************************************************************
+function booWho(bool) {
+  return typeof bool === "boolean" ? true : false;
+}
+console.log(booWho(null));
+console.log(booWho(true));
+// ****************************************************************************
+// Finders Keepers
+// ****************************************************************************
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) if (func(arr[i])) return arr[i];
+}
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
+// ****************************************************************************
+// Truncate a String
+// ****************************************************************************
+function truncateString(str, num) {
+  if (str.length > num) return str.substring(0, num) + "...";
+  return str;
+}
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+// ****************************************************************************
+// Repeat a String
+// ****************************************************************************
+function repeatStringNumTimes(str, num) {
+  if (num <= 0) return "";
+  const cad = str;
+  for (let i = 1; i < num; i++) {
+    str += cad;
+    console.log(i, str);
+  }
+  return str;
+}
+console.log(repeatStringNumTimes("abc", 3));
 
 // ****************************************************************************
 // Confirm the Ending
 // ****************************************************************************
 function confirmEnding(str, target) {
-  // return str.endsWith(target); // Respuesta rápida
-  // const regEx = new RegExp(`/` + target + `$/`);
-  console.log(regEx);
+  const regEx = new RegExp(target + "$");
   return regEx.test(str);
 }
 
-console.log(confirmEnding("Bastian", "n"));
-console.log(confirmEnding("Bastian", "n"));
-/*
+confirmEnding("Bastian", "n");
 
 // ****************************************************************************
 // Return Largest Numbers in Arrays
