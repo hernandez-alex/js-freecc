541ed063014aa0cbd563145c8373c1c4cd7f11e1
Alex Hernandez <62507090+hernandezaw@users.noreply.github.com>
2023-04-16 08:28:56 -0400

    update functional-prog.js

--- functional-prog.js
+++ functional-prog.js
@@ -1,10 +1,249 @@
 "use strict";
-console.log("alex");
 // ****************************************************************************
-// Return Part of an Array Using the slice Method
+// Apply Functional Programming to Convert Strings to URL Slugs
 // ****************************************************************************
-
+// Cambia solo el código debajo de esta línea
+function urlSlug(title) {
+  console.log(`r:`, title.trim().split(" ").join("-").toLowerCase());
+}
+// Cambia solo el código encima de esta línea
+urlSlug("         A Mind Needs Books Like A Sword Needs A Whetstone");
+urlSlug(" Winter Is  Coming");
 /*
+// ****************************************************************************
+// Combine an Array into a String Using the join Method
+// ****************************************************************************
+function sentensify(str) {
+  // Only change code below this line
+  return str.split(/[^a-zA-Z]/).join(" ");
+  // Only change code above this line
+}
+
+console.log(sentensify("May-the-force-be-with-you"));
+
+// ****************************************************************************
+// Split a String into an Array Using the split Method
+// ****************************************************************************
+function splitify(str) {
+  // Only change code below this line
+  return str.split(/[^a-zA-Z]/);
+  // Only change code above this line
+}
+
+console.log(splitify("Hello World,I-am code"));
+
+// ****************************************************************************
+// Return a Sorted Array Without Changing the Original Array
+// ****************************************************************************
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return arr.slice().sort((a, b) => a - b);
+  // Only change code above this line
+}
+
+console.log(nonMutatingSort(globalArray));
+
+// ****************************************************************************
+// Sort an Array Alphabetically using the sort Method
+// ****************************************************************************
+function alphabeticalOrder(arr) {
+  // Only change code below this line
+  return arr.sort((b, a) => (a > b ? 1 : a < b ? -1 : 0));
+  // Only change code above this line
+}
+
+console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
+
+// ****************************************************************************
+// Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+// ****************************************************************************
+const squareList = (arr) => {
+  // Only change code below this line
+  return arr.filter((elem, i, arr) => elem > 0 && elem % 1 === 0)
+    .map((elem, i, arr) => elem ** 2)
+  ;
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
+
+// ****************************************************************************
+// Use the reduce Method to Analyze Data
+// ****************************************************************************
+// The global variable
+const watchList = [
+  {
+    Title: "Inception",
+    Year: "2010",
+    Rated: "PG-13",
+    Released: "16 Jul 2010",
+    Runtime: "148 min",
+    Genre: "Action, Adventure, Crime",
+    Director: "Christopher Nolan",
+    Writer: "Christopher Nolan",
+    Actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+    Plot: "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+    Language: "English, Japanese, French",
+    Country: "USA, UK",
+    Awards: "Won 4 Oscars. Another 143 wins & 198 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    Metascore: "74",
+    imdbRating: "8.8",
+    imdbVotes: "1,446,708",
+    imdbID: "tt1375666",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "Interstellar",
+    Year: "2014",
+    Rated: "PG-13",
+    Released: "07 Nov 2014",
+    Runtime: "169 min",
+    Genre: "Adventure, Drama, Sci-Fi",
+    Director: "Christopher Nolan",
+    Writer: "Jonathan Nolan, Christopher Nolan",
+    Actors: "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    Plot: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    Language: "English",
+    Country: "USA, UK",
+    Awards: "Won 1 Oscar. Another 39 wins & 132 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+    Metascore: "74",
+    imdbRating: "8.6",
+    imdbVotes: "910,366",
+    imdbID: "tt0816692",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "The Dark Knight",
+    Year: "2008",
+    Rated: "PG-13",
+    Released: "18 Jul 2008",
+    Runtime: "152 min",
+    Genre: "Action, Adventure, Crime",
+    Director: "Christopher Nolan",
+    Writer:
+      "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+    Actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+    Plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+    Language: "English, Mandarin",
+    Country: "USA, UK",
+    Awards: "Won 2 Oscars. Another 146 wins & 142 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+    Metascore: "82",
+    imdbRating: "9.0",
+    imdbVotes: "1,652,832",
+    imdbID: "tt0468569",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "Batman Begins",
+    Year: "2005",
+    Rated: "PG-13",
+    Released: "15 Jun 2005",
+    Runtime: "140 min",
+    Genre: "Action, Adventure",
+    Director: "Christopher Nolan",
+    Writer:
+      "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+    Actors: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+    Plot: "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+    Language: "English, Urdu, Mandarin",
+    Country: "USA, UK",
+    Awards: "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+    Metascore: "70",
+    imdbRating: "8.3",
+    imdbVotes: "972,584",
+    imdbID: "tt0372784",
+    Type: "movie",
+    Response: "True",
+  },
+  {
+    Title: "Avatar",
+    Year: "2009",
+    Rated: "PG-13",
+    Released: "18 Dec 2009",
+    Runtime: "162 min",
+    Genre: "Action, Adventure, Fantasy",
+    Director: "James Cameron",
+    Writer: "James Cameron",
+    Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    Plot: "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    Language: "English, Spanish",
+    Country: "USA, UK",
+    Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    Poster:
+      "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    Metascore: "83",
+    imdbRating: "7.9",
+    imdbVotes: "876,575",
+    imdbID: "tt0499549",
+    Type: "movie",
+    Response: "True",
+  },
+];
+
+function getRating(watchList) {
+  // Only change code below this line
+  let averageRating = watchList
+    .filter((movie) => movie.Director === "Christopher Nolan")
+    .reduce((avg, movie, i, arr) => avg + movie.imdbRating / arr.length, 0);
+  // Only change code above this line
+  return averageRating;
+}
+
+console.log(getRating(watchList));
+
+// ****************************************************************************
+// Add Elements to the End of an Array Using concat Instead of push
+// ****************************************************************************
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+  // Only change code above this line
+}
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
+console.log(first, second, nonMutatingPush(first, second));
+console.log(first, 0, nonMutatingPush(first, 0, nonMutatingPush(first, 0)));
+
+// ****************************************************************************
+// Combine Two Arrays Using the concat Method
+// ****************************************************************************
+function nonMutatingConcat(original, attach) {
+  // Only change code below this line
+  return original.concat(attach);
+  // Only change code above this line
+}
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
+console.log(first, second, nonMutatingConcat(first, second));
+
+// ****************************************************************************
+// Remove Elements from an Array Using slice Instead of splice
+// ****************************************************************************
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  // return cities.splice(3);
+  return cities.slice(0, 3);
+  // Only change code above this line
+}
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+console.log(nonMutatingSplice(inputCities));
+
 // ****************************************************************************
 // Return Part of an Array Using the slice Method
 // ****************************************************************************
