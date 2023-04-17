3ff6439122bbe3dc18f45921914132d806cd0ada
Alex Hernandez <62507090+hernandezaw@users.noreply.github.com>
2023-04-17 08:33:53 -0400

    updating int-alg-scription.js

--- int-alg-scripting.js
+++ int-alg-scripting.js
@@ -1,9 +1,36 @@
 "use strict";
 // ****************************************************************************
-//
+// Wherefore art thou
 // ****************************************************************************
-
 /*
+
+// ****************************************************************************
+// Seek and Destroy
+// ****************************************************************************
+function destroyer(arr, ...f) {
+  return arr.filter((el) => !f.includes(el));
+}
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+// ****************************************************************************
+// Diff Two Arrays
+// ****************************************************************************
+function diffArray(arr1, arr2) {
+  const newArr = [];
+  arr1.map((elem) => {
+    if (!arr2.includes(elem)) newArr.push(elem);
+  });
+  arr2.map((elem) => {
+    if (!arr1.includes(elem)) newArr.push(elem);
+  });
+
+  // arr1.map((elem) => (!arr2.includes(elem) ? newArr.push(elem) : false));
+  // arr2.map((elem) => (!arr1.includes(elem) ? newArr.push(elem) : false));
+  return newArr;
+}
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
 // ****************************************************************************
 // Sum All Numbers in a Range
 // ****************************************************************************
